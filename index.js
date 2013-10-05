91593d88aea8282614fd1efb4f5141713897e2a4
Michael Blais Younkin <younkin.michael@gmail.com>
2013-10-05 23:16:19 +0000

    Hammer out more of the server

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ var app = express();
 
 // Create a new REST API client to make authenticated requests against the
 // twilio back end
-//var client = new twilio.RestClient(
-//  config.twilio.sid,
-//  config.twilio.auth
-//);
+var client = new twilio.RestClient(
+  config.twilio.sid,
+  config.twilio.auth
+);
 
-app.get('/incomingcall', function(req, res) {
+app.post('/incomingcall', function(req, res) {
+	console.log('received incoming call');
   var resp = new twilio.TwimlResponse();
 
   resp.say({voice: 'woman'}, 'Thank you for your call to team 42.');
@@ -23,4 +24,10 @@ app.get('/incomingcall', function(req, res) {
   res.end(resp.toString());
 });
 
-app.listen(config.web);
+app.post('/incomingtext', function(req, res) {
+  console.log('received incoming text');
+  res.send(200, '');
+});
+
+app.listen(config.web.port);
+console.log('Listening on port ' + config.web.port);
