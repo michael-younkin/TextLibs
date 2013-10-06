3279db2cb47b0e307778693370ec596fad751e45
Michael Blais Younkin <younkin.michael@gmail.com>
2013-10-06 01:25:22 +0000

    Accept nouns

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var twilio = require('twilio');
 var config = require('./config');
 var _ = require('lodash');
 var express = require('express');
-var app = express();
-
+var app = express(); 
 // Create a new REST API client to make authenticated requests against the
 // twilio back end
 var client = new twilio.RestClient(
@@ -13,8 +12,8 @@ var client = new twilio.RestClient(
   config.twilio.auth
 );
 
-var phase = 'signup';
-var numbers = [];
+var pendingLibs = {};
+var results = [];
 
 app.configure(function() {
   app.use(express.bodyParser());
@@ -22,31 +21,62 @@ app.configure(function() {
 
 app.post('/incomingcall', function(req, res) {
 	console.log('received incoming call');
-  var resp = new twilio.TwimlResponse();
-  if (phase === 'signup') {
-    numbers.push(req.body.From);
-    numbers = _.uniq(numbers);
-    
-    if (numbers.length === config.collectSize) {
-      console.log('Signup completed');
-      console.dir(numbers);
-      phase = 'libs';
-    }
-    resp.say({voice: 'female'}, 'You have registered');
-  } else if (phase === 'libs') {
-    resp.say({voice: 'male'}, 'We have completed the signup phase.' + 
-             ' Try again later');
-  }
-  res.writeHead(200, {
-    'Content-Type': 'text/xml'
-  });
-  res.end(resp.toString());
 });
 
 app.post('/incomingtext', function(req, res) {
   console.log('received incoming text');
-  res.send(200, '');
+  var incomingNumber = req.body.From;
+  if (pendingLibs[incomingNumber]) {
+    handleInsertion(incomingNumber, req.body.Body, function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  } else if (libsAreRemaining()) {
+    requestLib(incomingNumber, function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  } else {
+    sendSMS(
+      incomingNumber,
+      'All libs are taken, please try again in a few  minutes.',
+      function(err) {
+        if (err) {
+          throw err;
+        }
+      }
+    );
+  }
 });
+
+function libsAreRemaining() {
+  return true;
+}
+
+function sendSMS(number, msg, callback) {
+  client.sendSms({
+    to: number,
+    from: config.twilio.number,
+    body: msg
+  }, callback);
+}
+
+function handleInsertion(incomingNumber, contents, callback) {
+  results.push(contents);
+  console.log('Current libs');
+  console.dir(results);
+  pendingLibs[incomingNumber] = undefined;
+  callback();
+}
+
+function requestLib(incomingNumber, callback) {
+  sendSMS(incomingNumber, 'Please send us a noun.', function(err) {
+    pendingLibs[incomingNumber] = 'noun';
+    callback(err);
+  });
+}
 
 app.listen(config.web.port);
 console.log('Listening on port ' + config.web.port);
